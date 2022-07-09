345a3838f9afb03c76eaf95ac44813e43b9531d1
Ashutosh Potdar <72146279+Ashutosh-27@users.noreply.github.com>
2022-07-09 08:42:20 +0530

    Update landing.js

--- js/landing.js
+++ js/landing.js
@@ -40,6 +40,66 @@ themeButton.addEventListener('click', () => {
 })
 
 
-    // ============ SCROLL REVEL ===========
 
+
+const TypeWriterEffect = function(txtElement,words,wait=3000){
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait,10);//radix-10 defines decimal number system
+    this.typewrd();
+    this.isDeleting = false;
+}
+
+TypeWriterEffect.prototype.typewrd =function(){
+
+    const currentwrd = this.wordIndex % this.words.length;
+    //assume currentwrd is 0, 0%3=0, thus fullText = this.words[0]
+    const fullText = this.words[currentwrd];
+
+    if(this.isDeleting){
+        this.txt = fullText.substring(0,this.txt.length-1)
+    }
+    else{
+        this.txt = fullText.substring(0,this.txt.length +1)
+    }
+
+    this.txtElement.innerHTML = `<span class='txt'>${this.txt}</span>`;
+
+    let typeSpeed = 300;
+    if(this.isDeleting){
+        typeSpeed /= 2;
+
+    }
+
+    //if words is complete
+    if(!this.isDeleting && this.txt === fullText){
+        typeSpeed = this.wait;
+        this.isDeleting = true
+    }
+    else if(this.isDeleting && this.txt==''){
+        this.isDeleting=false;
+
+        //MOve to next words
+        this.wordIndex++;
+
+        //pause before typing next word
+        typeSpeed = 500;
+    }
+
+    setTimeout(()=>this.typewrd(),typeSpeed);
+}
+
+
+
+document.addEventListener('DOMContentLoaded',initialize);
+
+
+function initialize(){const txtElement = document.getElementById('type-txt');
+const words = JSON.parse(txtElement.getAttribute('data-words'));
+const wait = txtElement.getAttribute('data-wait');
+
+new TypeWriterEffect(txtElement,words,wait)
+}
 
